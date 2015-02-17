076303d305fe449c3c68e7b261f19e496a1a506d
Dasha <dzakharova@twinfield.com>
2015-02-17 09:42:03 +0200

    HW

---
+++ main.js
@@ -0,0 +1,14 @@
+//var genre = 'fantasy';
+//var genre = 'novel';
+//var genre = 'comics';
+var genre = 'biographical';
+
+if (genre == 'fantasy'){
+	console.log('The Hobbit');
+} else if (genre == 'novel'){
+	console.log('The Notebook');
+} else if (genre == 'biographical'){
+	console.log('Leaving Before the Rains Come');
+} else {
+	console.log('I dont know');
+};
